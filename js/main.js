ce69437e54c54ec2a8810dbc04963f054290e66d
Synapse-ITA <puggillovalerio@gmail.com>
2023-11-21 14:12:48 +0100

    milestone 0 done

---
+++ js/main.js
@@ -0,0 +1,47 @@
+
+// MILESTONE 0: CREARE L'ARRAY DI OGGETTI CON LE INFORMAZIONI FORNITE NELLA TABELLA QUI SOTTO:
+
+let teamMembers = [
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        photoUrl: "",
+    },
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        photoUrl: "",
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        photoUrl: "",
+    },
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        photoUrl: "",
+    },
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        photoUrl: "",
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        photoUrl: "",
+    },
+];
+
+// MILESTONE 1: STAMPARE SU CONSOLE LE INFORMAZIONI DI NOME, RUOLO E LA STRINGA DELLA FOTO
+
+
+
+
+
+
+
+
+
+// MILESTONE 2: STAMPARE LE STESSE INFORMAZIONI SU DOM SOTTOFORMA DI STRINGHE
