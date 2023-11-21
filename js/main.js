dc88cb9e4935c120afb35fbf78b18096e7ee14fa
Synapse-ITA <puggillovalerio@gmail.com>
2023-11-21 14:41:05 +0100

    all milestones done

--- js/main.js
+++ js/main.js
@@ -36,12 +36,41 @@ let teamMembers = [
 
 // MILESTONE 1: STAMPARE SU CONSOLE LE INFORMAZIONI DI NOME, RUOLO E LA STRINGA DELLA FOTO
 
-
-
-
-
-
-
-
+printArrayTeam(teamMembers);
 
 // MILESTONE 2: STAMPARE LE STESSE INFORMAZIONI SU DOM SOTTOFORMA DI STRINGHE
+
+let teamList = document.getElementById('teamList');
+
+for(let i = 0; i < teamMembers.length; i++) {
+    let li = document.createElement('li');
+    li.textContent = `${teamMembers[i].name} - Ruolo: ${teamMembers[i].role}`;
+    teamList.appendChild(li);
+}
+
+// SEZIONE FUNZIONI
+
+function printObject(object) {
+    let string = "";
+
+    string += "{\n";
+
+    for (let key in object) {
+        string += "\t " + key + ": " + object[key] + "\n";
+    }
+
+    string += "}";
+
+    console.log(string);
+
+}
+
+function printArrayTeam(arrayTeam) {
+    for (let i = 0; i < arrayTeam.length; i++) {
+
+        let team = arrayTeam[i];
+
+        console.log("Il membro del Team n° " + (i + 1) + " è:");
+        printObject(team);
+    }
+}
